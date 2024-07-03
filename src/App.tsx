963a457bd035c30f4e97aa0718a5e74b34fbc1d9
Maxwell Moura <maxwellcmoura@gmail.com>
2024-07-03 16:12:21 -0300

    Atualização 03.07.2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+//React
+import React, { useState, useEffect } from 'react'
 //Components
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -7,19 +8,39 @@ import styles from './App.module.css'
 //Formulario
 import Form from './components/Form'
 import Lista from './components/Lista'
+//Interface
+import { ITask } from './interface/Task'
 
 function App() {
+  //Variável da lista de tarefas
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  //Função para adicionar uma nova tarefa
+  const addTask = (task: ITask) => {
+    setTasks([
+      ...tasks,
+      task
+    ])
+  }
+  // useEffect para monitorar mudanças na lista de tarefas
+  useEffect(() => {
+    console.log('Lista de tarefas atualizada:', tasks);
+  }, [tasks]);
+
    return (
    <div>
     <Header />
    <main className={styles.main}>
     <div>
       <h2>O que você vai fazer?</h2>
-      <Form btnText='Criar Tarefa' />
+      <Form btnText='Criar Tarefa' 
+      taskList={tasks}
+      addTask={addTask} 
+      setTaskList={setTasks} />
     </div>
     <div>
       <h2>Suas Tarefas</h2>
-      <Lista />
+      <Lista tasks={tasks}/>
     </div>
    </main>
     <Footer />
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,67 @@
+//React, useState(Manuzear o estado), ChangeEvent(Manuzear o evento), FormEvent(Submeter o Formulario) e useEfect()
+import React, {useState, ChangeEvent, FormEvent, useEffect} from 'react'
 import styles from './Form.module.css'
+//Interface
+import { ITask } from '../interface/Task'
 
-interface Props  {
-  btnText: string
+interface Props {
+  btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+  addTask: (task: ITask) => void;
 }
 
-const Form = ({btnText}: Props) => {
+const TaskForm = ({ btnText, taskList, addTask, setTaskList }: Props) => {
+  // Criação de variáveis
+
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>('');
+  const [dificuldade, setDificuldade] = useState<number>(0);
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const id = Math.floor(Math.random() * 1000);
+    const newTask: ITask = { id, title, dificuldade };
+    setTaskList!([...taskList, newTask]); //União da taskList e a newTask
+    addTask(newTask);
+    setTitle(''); //Limpar o campo de titulo após a submissão
+    setDificuldade(0); //Limpar o campo de dificuldade
+    console.log(taskList)
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setDificuldade(parseInt(e.target.value));
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={addTaskHandler} className={styles.form}>
       <div className={styles.input_container}>
-        <label htmlFor="title">Titulo:</label>
-        <input type="text" name='title' placeholder='Titulo da Tarefa' />
+        <label htmlFor="title">Título:</label>
+        <input
+          type="text"
+          name="title"
+          placeholder="Título da Tarefa"
+          onChange={handleChange}
+          value={title}
+        />
       </div>
       <div className={styles.input_container}>
-        <label htmlFor="title">Dificuldade:</label>
-        <input type="text" name='dificuldade' placeholder='Dificuldade da Tarefa' />
+        <label htmlFor="dificuldade">Dificuldade:</label>
+        <input
+          type="number"
+          name="dificuldade"
+          placeholder="Dificuldade da Tarefa"
+          onChange={handleChange}
+          value={dificuldade}
+        />
       </div>
       <input type="submit" value={btnText} />
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default TaskForm;
